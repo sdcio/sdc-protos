231f052b6d952cff0103f5b5fd216117cf994a68
steiler <github@vahlenkamp.net>
2024-11-28 18:22:38 +0100

    Indicate in the mandatories if the child is state or config

---
+++ sdcpb/container_schema_additions.go
@@ -0,0 +1,25 @@
+package schema_server
+
+func (x *ContainerSchema) GetMandatoryChildrenConfig() []*MandatoryChild {
+	var result []*MandatoryChild
+	if x != nil {
+		for _, c := range x.MandatoryChildren {
+			if !c.GetIsState() {
+				result = append(result, c)
+			}
+		}
+	}
+	return result
+}
+
+func (x *ContainerSchema) GetMandatoryChildrenState() []*MandatoryChild {
+	var result []*MandatoryChild
+	if x != nil {
+		for _, c := range x.MandatoryChildren {
+			if c.GetIsState() {
+				result = append(result, c)
+			}
+		}
+	}
+	return result
+}
