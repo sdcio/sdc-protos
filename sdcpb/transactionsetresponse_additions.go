c323961bd7ad05becc369ef03c7afee2e9a76f15
steiler <github@vahlenkamp.net>
2025-03-12 12:14:26 +0100

    add a failed function to the transactionresponse

---
+++ sdcpb/transactionsetresponse_additions.go
@@ -0,0 +1,23 @@
+package schema_server
+
+import "fmt"
+
+func (t *TransactionSetResponse) GetErrors() []string {
+	result := []string{}
+
+	for intentName, intentErrs := range t.GetIntents() {
+		for _, e := range intentErrs.GetErrors() {
+			result = append(result, fmt.Sprintf("intent: %s, %v", intentName, e))
+		}
+	}
+	return result
+}
+
+func (t *TransactionSetResponse) Failed() bool {
+	for _, intentErrs := range t.GetIntents() {
+		if intentErrs.Failed() {
+			return true
+		}
+	}
+	return false
+}
